53cd328785f6178dd16a5e45fdf1258bfe2ad75c
Tanish Singh <tanishsingh6005@gmail.com>
2026-04-27 02:09:27 +0530

    Time: 29 ms (70.95%), Space: 14.8 MB (10.89%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def connect(self, root):
+        if not root:
+            return None
+        
+        curr = root
+        
+        while curr:
+            dummy = Node(0)
+            tail = dummy
+            
+            while curr:
+                if curr.left:
+                    tail.next = curr.left
+                    tail = tail.next
+                if curr.right:
+                    tail.next = curr.right
+                    tail = tail.next
+                curr = curr.next
+            
+            curr = dummy.next
+        
+        return root
